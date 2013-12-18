a26b7a6f9fc30946d6e05d493730877939da7495
nemoshlag <shlagman@gmail.com>
2013-12-18 21:12:31 +0200

    Added netbeans project and a bunch of TODOs

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // What do we want to do?
+        //
+        // Receive json from client 
+        // Lets start with premade json
+        {
+        $x = 5;
+        //$y = "results:{"key": "value"}";
+        // get value from json
+        // 
+        // WIKIPEDIA
+        // get from wikipedia api images (lets start with one)
+        // attach value to wikipedia api for clear text
+        // 
+        // ALCHEMY
+        // send clear text to alchemyAPI
+        // receive consept and keywords (I believe we can get as json)
+        // 
+        // send json to clien
+        ?>
+    </body>
+</html>
